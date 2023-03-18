06368ef85fd85ab8355dda40610921726d5bfb7d
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-18 23:19:28 +0530

    +: use karla

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">hi links</h1>
+      <h1 className="text-3xl font-black underline">Hi Links</h1>
     </div>
   );
 }
