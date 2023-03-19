5bbf4a6b9ffe744868ab36e9da650c53ea9f157d
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-19 18:39:22 +0530

    -: remove sql and add redis

--- core/db/client.ts
+++
@@ -1,6 +0,0 @@
-import { connect } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
-
-const connection = connect({ url: process.env.DATABASE_URL });
-
-export const db = drizzle(connection);
--- core/db/schema.ts
+++
@@ -1,1 +0,0 @@
-// schema goes here
