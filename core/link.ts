54a5f3a4b6805ebc9739aaeac35b96f40bebff37
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-20 00:05:02 +0530

    +: add link functions

---
+++ core/link.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+import { redis } from "./redis";
+import { getEmojiString } from "./emoji";
+
+export const LinkType = z.union([z.literal("emoji"), z.literal("sketchy")]);
+
+export const LinkSchema = z.object({
+  link: z.string().url(),
+  type: LinkType,
+});
+
+export type LinkType = z.infer<typeof LinkType>;
+export type Link = z.infer<typeof LinkSchema>;
+
+export async function createLink({ link, type }: Link): Promise<void> {
+  switch (type) {
+    case "emoji":
+      const emojiString = getEmojiString(4);
+
+      const didSet = await redis.set(emojiString, link, { nx: true });
+
+      if (!didSet) {
+        return createLink({ link, type });
+      }
+  }
+}
