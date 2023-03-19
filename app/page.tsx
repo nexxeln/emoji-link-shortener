67f4f10c7a4057ebf4b6fa65b5ce71e359df8be9
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-19 15:36:28 +0530

    +: home page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-black underline">Hi Links</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-5xl font-black uppercase tracking-wider">spores</h1>
+      <p className="text-xl font-semibold tracking-wide text-secondary">
+        A Different Link Shortener
+      </p>
+      <div className="pt-6" />
+      <div className="flex gap-8 text-lg">
+        <Link
+          href="/emoji"
+          className="underline decoration-secondary decoration-1 underline-offset-4 hover:decoration-primary"
+        >
+          Emoji
+        </Link>
+        <Link
+          href="/sketchy"
+          className="underline decoration-secondary decoration-1 underline-offset-4 hover:decoration-primary"
+        >
+          Sketchy
+        </Link>
+      </div>
     </div>
   );
 }
