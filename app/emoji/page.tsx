b85171ab38907bf302bb0ca5701470e5120d4000
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-19 21:42:39 +0530

    *: extract form into reusable component

--- app/emoji/page.tsx
+++ app/emoji/page.tsx
@@ -1,25 +1,12 @@
+// client component
+import CreateLinkForm from "~~/components/Form";
+
 export default function EmojiPage() {
   return (
     <div className="flex min-h-screen w-screen flex-col items-center justify-center">
-      <h1 className="text-4xl">😱😳🥴😎</h1>
+      <h1 className="text-4xl">😱😳🥴🥵</h1>
       <div className="pt-8" />
-      <form>
-        <div className="flex flex-col gap-4">
-          <input
-            type="url"
-            name="url"
-            id="url"
-            placeholder="https://link-you-wanna-shorten.com"
-            className="w-[17rem] border-b border-b-secondary bg-dark focus:border-b-primary focus:outline-none"
-          />
-          <button
-            type="submit"
-            className="font-semibold underline decoration-secondary decoration-1 underline-offset-4 hover:decoration-primary"
-          >
-            Shorten With Emojis
-          </button>
-        </div>
-      </form>
+      <CreateLinkForm type="emoji" />
     </div>
   );
 }
---
+++ components/Form.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+
+type FormType = "emoji" | "sketchy";
+
+function getButtonText(type: FormType): string {
+  switch (type) {
+    case "emoji":
+      return "Shorten With Emojis";
+    case "sketchy":
+      return "Create Sketchy Link";
+  }
+}
+
+export default function CreateLinkForm({ type }: { type: FormType }) {
+  const router = useRouter();
+  const [link, setLink] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
+  const [isPending, startTransition] = useTransition();
+
+  const isMutating = isFetching || isPending;
+
+  async function handleSubmit() {
+    setIsFetching(true);
+
+    await fetch("/api/shorten", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ link, type }),
+    });
+
+    setIsFetching(false);
+
+    startTransition(() => router.refresh());
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex flex-col gap-4">
+        <input
+          type="url"
+          name="url"
+          id="url"
+          autoComplete="off"
+          placeholder="https://link-you-wanna-shorten.com"
+          className="w-[17rem] border-b border-b-secondary bg-dark focus:border-b-primary focus:outline-none"
+          required
+          value={link}
+          onChange={(event) => setLink(event.target.value)}
+        />
+        <button
+          type="submit"
+          disabled={isMutating}
+          className={`font-semibold underline decoration-secondary decoration-1 underline-offset-4 transition-colors hover:decoration-primary ${
+            isMutating ? "cursor-not-allowed opacity-70" : ""
+          }`}
+        >
+          {getButtonText(type)}
+        </button>
+      </div>
+    </form>
+  );
+}
