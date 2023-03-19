08cbe81b8d1faf4574b49ab9134d959ce931f7f5
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-19 21:42:55 +0530

    ^: update home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,19 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-5xl font-black uppercase tracking-wider">spores</h1>
       <p className="text-xl font-semibold tracking-wide text-secondary">
-        A Different Link Shortener
+        An Iconoclastic Link Shortener
       </p>
       <div className="pt-6" />
       <div className="flex gap-8 text-lg">
         <Link
           href="/emoji"
-          className="underline decoration-secondary decoration-1 underline-offset-4 hover:decoration-primary"
+          className="underline decoration-secondary decoration-1 underline-offset-4 transition-colors hover:decoration-primary"
         >
           Emoji
         </Link>
         <Link
           href="/sketchy"
-          className="underline decoration-secondary decoration-1 underline-offset-4 hover:decoration-primary"
+          className="underline decoration-secondary decoration-1 underline-offset-4 transition-colors hover:decoration-primary"
         >
           Sketchy
         </Link>
