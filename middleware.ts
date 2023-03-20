2b68eb3af833b00b2909cf58891f02c0dff153d6
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-20 10:18:23 +0530

    *: use import alias

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getLink } from "./core/link";
+import { getLink } from "~~/core/link";
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
