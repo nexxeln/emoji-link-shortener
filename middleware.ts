a511fc231ef0610b65f5cc839566fabd3c296362
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-20 10:07:01 +0530

    +: add middle ware to redirect

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getLink } from "./core/link";
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const link = await getLink(decodeURI(pathname.slice(1)));
+
+  return link
+    ? NextResponse.redirect(new URL(link))
+    : NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - emoji (emoji route)
+     * - sketchy (sketchy route)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|emoji|sketchy).*)",
+  ],
+};
